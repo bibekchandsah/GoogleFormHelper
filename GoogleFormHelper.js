27ab8bd176dbcba6df97f99c8674636abd76b1b2
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2023-12-11 01:29:29 +0530

    added timer
    added timer in the form

--- GoogleFormHelper.js
+++ GoogleFormHelper.js
@@ -183,95 +183,96 @@
 
 
 
-     // Prompt user for countdown timer
-var countdownTime = prompt("How many minutes do you want to serve this website?");
-var countdownSeconds = countdownTime * 60;
+    // Prompt user for countdown time
+    var countdownTime = prompt("How many minutes do you want to serve this website?");
+    var countdownSeconds = countdownTime * 60;
 
-// Create countdown timer element
-var timerElement = document.createElement('div');
-timerElement.id = 'countdown-timer';
-document.body.appendChild(timerElement);
+    // Create countdown timer element
+    var timerElement = document.createElement('div');
+    timerElement.id = 'countdown-timer';
+    document.body.appendChild(timerElement);
 
-// Add inline CSS styles
-timerElement.style.position = 'fixed';
-timerElement.style.top = '10px';
-timerElement.style.right = '10px';
-timerElement.style.fontSize = '16px';
-timerElement.style.backgroundColor = '#fff';
-timerElement.style.padding = '10px';
-timerElement.style.border = '1px solid #ddd';
-timerElement.style.borderRadius = '5px';
-timerElement.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
-timerElement.style.zIndex = '9999';
-timerElement.style.cursor = 'pointer'; // Set cursor to pointer
+    // Add inline CSS styles
+    timerElement.style.position = 'fixed';
+    timerElement.style.top = '10px';
+    timerElement.style.right = '10px';
+    timerElement.style.fontSize = '16px';
+    timerElement.style.backgroundColor = '#fff';
+    timerElement.style.padding = '10px';
+    timerElement.style.border = '1px solid #ddd';
+    timerElement.style.borderRadius = '5px';
+    timerElement.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+    timerElement.style.zIndex = '9999';
+    timerElement.style.cursor = 'pointer'; // Set cursor to pointer
 
-// Create audio element
-var audio = new Audio('https://cdn.pixabay.com/audio/2021/08/04/audio_c668156e64.mp3');
+    // Create audio element
+    var audio = new Audio('https://cdn.pixabay.com/audio/2021/08/04/audio_c668156e64.mp3');
 
-// Flags to track the play/pause state
-var isPlaying = false;
-var isMusicPlaying = false;
+    // Flags to track the play/pause state
+    var isPlaying = false;
+    var isMusicPlaying = false;
 
-// Event listener for timer click
-timerElement.addEventListener('click', function () {
-    // Check if the countdown is less than or equal to 60 seconds (alert time)
-    if (countdownSeconds <= 60) {
-        if (isPlaying) {
-            // Pause the audio
+    // Event listener for timer click
+    timerElement.addEventListener('click', function () {
+        // Check if the countdown is less than or equal to 60 seconds (alert time)
+        if (countdownSeconds <= 60) {
+            if (isPlaying) {
+                // Pause the audio
+                audio.pause();
+            } else {
+                // Play the audio
+                audio.play();
+            }
+
+            // Toggle the play/pause state
+            isPlaying = !isPlaying;
+        }
+    });
+
+    // Update timer every second
+    var countdownInterval = setInterval(function () {
+        var minutes = Math.floor(countdownSeconds / 60);
+        var seconds = countdownSeconds % 60;
+
+        // Update timer display
+        timerElement.innerHTML = 'Time Remaining: ' + minutes + 'm ' + seconds + 's';
+
+        // Check if the countdown is less than or equal to 60 seconds (alert time)
+        if (countdownSeconds <= 60) {
+            // Check if the music should play automatically
+            if (!isMusicPlaying) {
+                // Play the audio
+                audio.play();
+
+                // Set the flag to true
+                isMusicPlaying = true;
+            }
+
+            // Adjust font size and padding dynamically
+            var fontSize = 16 + Math.sin(countdownSeconds * 0.1) * 4; // Adjust the multiplier for speed
+            var padding = 10 + Math.sin(countdownSeconds * 0.1) * 5; // Adjust the multiplier for speed
+
+            timerElement.style.fontSize = fontSize + 'px';
+            timerElement.style.padding = padding + 'px';
+        }
+
+        // Check if the countdown reached 0
+        if (countdownSeconds <= 0) {
+            clearInterval(countdownInterval);
+
+            // Stop the audio
             audio.pause();
-        } else {
-            // Play the audio
-            audio.play();
+            audio.currentTime = 0;
+
+            // Remove the timer element
+            document.body.removeChild(timerElement);
         }
 
-        // Toggle the play/pause state
-        isPlaying = !isPlaying;
-    }
-});
-
-// Update timer every second
-var countdownInterval = setInterval(function () {
-    var minutes = Math.floor(countdownSeconds / 60);
-    var seconds = countdownSeconds % 60;
-
-    // Update timer display
-    timerElement.innerHTML = 'Time Remaining: ' + minutes + 'm ' + seconds + 's';
-
-    // Check if the countdown is less than or equal to 60 seconds (alert time)
-    if (countdownSeconds <= 60) {
-        // Check if the music should play automatically
-        if (!isMusicPlaying) {
-            // Play the audio
-            audio.play();
-
-            // Set the flag to true
-            isMusicPlaying = true;
-        }
-
-        // Adjust font size and padding dynamically
-        var fontSize = 16 + Math.sin(countdownSeconds * 0.1) * 4; // Adjust the multiplier for speed
-        var padding = 10 + Math.sin(countdownSeconds * 0.1) * 5; // Adjust the multiplier for speed
-
-        timerElement.style.fontSize = fontSize + 'px';
-        timerElement.style.padding = padding + 'px';
-    }
-
-    // Check if the countdown reached 0
-    if (countdownSeconds <= 0) {
-        clearInterval(countdownInterval);
-        
-        // Stop the audio
-        audio.pause();
-        audio.currentTime = 0;
-
-        // Remove the timer element
-        document.body.removeChild(timerElement);
-    }
-
-    // Decrease countdown seconds
-    countdownSeconds--;
-}, 1000);
+        // Decrease countdown seconds
+        countdownSeconds--;
+    }, 1000);
 
 
-    
+
+
 })();
